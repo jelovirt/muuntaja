44379d62a202eeb35efbf303790fc2823dfc3def
Jarno Elovirta <jarno@elovirta.com>
2010-09-23 14:50:19 +0300

    Add SBT build.

---
+++ project/build/Build.scala
@@ -0,0 +1,19 @@
+import sbt._
+
+class MuuntajaProject(info: ProjectInfo) extends DefaultProject(info) {
+  
+  val snapshots = ScalaToolsSnapshots
+  
+  val XercesImpl = "xerces" % "xercesImpl" % "2.9.1" 
+  val XmlResolver = "xml-resolver" % "xml-resolver" % "1.2" 
+  val Xom = "xom" % "xom" % "1.2.5" 
+  val Scalatest = "org.scalatest" % "scalatest" % "1.2" 
+  val CommonsIo = "commons-io" % "commons-io" % "1.4" 
+  val ScalaLibrary = "org.scala-lang" % "scala-library" % "2.8.0" 
+  val Junit = "junit" % "junit" % "4.7" 
+  val WoodstoxCoreAsl = "org.codehaus.woodstox" % "woodstox-core-asl" % "4.0.7" 
+  val Ant = "org.apache.ant" % "ant" % "1.8.1" 
+  
+  override def compileOrder = CompileOrder.ScalaThenJava
+
+}
